05d7bb82245d5ba3e8d555c7e5fff7dc0e71a434
jd3b <debojyotijha23@gmail.com>
2024-01-31 17:00:46 +0530

    productsandcat

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,88 +1,76 @@
 import prisma from "@/app/db/db";
-import { ProductSchema } from "@/app/validators/validator";
+import { CategorySchema } from "@/app/validators/validator";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function GET(request: Request) {
-  const products = await prisma.product.findMany();
+  const categories = await prisma.category.findMany();
   
-  return Response.json(products);
+  return Response.json(categories);
 }
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const {searchParams}=new URL(request.url);
-  const query = Number(searchParams.get('categoryid'))
   try {
-    const { name, desc, summary, sellingPrice, costPrice, isFeatured } =
-      ProductSchema.parse(body);
-    const pdata = {
+    const { name, desc, slug } =
+      CategorySchema.parse(body);
+    const cdata = {
       
       name,
       desc,
-      summary,
-      sellingPrice,
-      costPrice,
-      isFeatured,
-      
+      slug,
     };    
     // console.log(pdata);
-    const createdProduct =await prisma.product.create({data:{...pdata,categoryid:query}})
+    const createdCategory =await prisma.category.create({data:cdata});
     // console.log(createdProduct);
     // not correct returning
     const options = { status: 200}; 
-    return Response.json(createdProduct, options);
+    return Response.json(createdCategory, options);
   } catch (error) {
-    console.log(error);
-    
     return new NextResponse('Unprocessable entity',{status:422});
   }
 }
 //Update Products
 export async function PUT(request: Request) {
   const {searchParams}=new URL(request.url);
-  const query1 = searchParams.get('pid')
+  const query = searchParams.get('cid')
   const body = await request.json();
-  const query2 = searchParams.get('cid')
   try {
     
-    const { name, desc, summary, sellingPrice, costPrice, isFeatured } =
-      ProductSchema.parse(body);
-    const pdata = {
+    const { name, desc, slug } =
+      CategorySchema.parse(body);
+    const cdata = {
       
       name,
       desc,
-      summary,
-      sellingPrice,
-      costPrice,
-      isFeatured,
+      slug,
     };    
     // console.log(pdata);
 
-    const updatedProduct =await prisma.product.update({where:{pid:Number(query1)},data:pdata});
+    const updatedCategory =await prisma.category.update({where:{cid:Number(query)},data:cdata});
     // console.log(createdProduct);
     // not correct returning
     const options = { status: 200}; 
-    return Response.json(updatedProduct, options);
+    return Response.json(updatedCategory, options);
   } catch (error) {
     return new NextResponse('Unprocessable entity',{status:422});
   }
 }
 export async function DELETE(request: Request) {
   const {searchParams}=new URL(request.url);
-  const query = searchParams.get('pid')
+  const query = searchParams.get('cid')
   const body = await request.json();
   try {
     
     
     // console.log(pdata);
 
-    const deleteProduct =await prisma.product.delete({where:{pid:Number(query)}});
+    const deleteCategory =await prisma.category.delete({where:{cid:Number(query)}});
     // console.log(createdProduct);
     // not correct returning
     const options = { status: 200}; 
-    return Response.json(deleteProduct, options);
+    return Response.json(deleteCategory, options);
   } catch (error) {
     return new NextResponse('Unprocessable entity',{status:422});
   }
--- app/validators/validator.ts
+++ app/validators/validator.ts
@@ -7,4 +7,9 @@ export const ProductSchema = z.object({
   costPrice: z.number(),
   isFeatured: z.boolean(),
 });
+export const CategorySchema = z.object({
+  name : z.string(),
+  desc : z.string(),
+  slug : z.string(),
+});
 
